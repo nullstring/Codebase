c3b86f5e46d569d73cf24c1fdbabafd592fc490b
Harsh Mehta <harsh.version.1@gmail.com>
2013-08-08 19:11:52 +0530

    something

---
+++ spoj/CRSCNTRY.cpp
@@ -0,0 +1,115 @@
+//Harsh Mehta
+
+//library includes 
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>//setprecision()
+#include <string>//s.length(),s.find("string"),s.find_last_of('char'),s.replace(from_pos,number_of_chars_to_replace,"string to replace"),s.c_str();
+#include <cstring>//strcpy(s1,s2),strcmp(s1,s2),strcmpi(s1,s2),strstr(s1,s2)  returns address location of s2,strlen()
+#include <cmath>//sin,cosec,sinh,acos,sqrt,pow,sort,atoi,atof,atol,ceil,exp,itoa,sprintf,modf
+#include <cctype>//isdigit(),isalnum(),isspace(),ispunct()
+
+using namespace std;
+// Usefull macros
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef pair<int,int> ii;
+typedef vector<string> vs;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+ 
+const int INF = (int) 1e9;
+const long long INF64 = (long long) 1e18;
+const long double eps = 1e-9;
+const long double pi = 3.14159265358979323846;
+
+#define pb push_back
+#define mp make_pair
+#define sz(a) (int)(a).size()
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+ 
+#define forn(i,n) for (int i=0; i<int(n); ++i)
+#define fornd(i,n) for (int i=int(n)-1; i>=0; --i)
+#define forab(i,a,b) for (int i=int(a); i<int(b); ++i)
+
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++) 
+
+#define present(c,x) ((c).find(x) != (c).end())  // for set/map etc 
+#define cpresent(c,x) (find(all(c),x) != (c).end())  // for vector 
+
+int a[2005];
+int b[2005];
+int dp[2005][2005];
+int lcs(int an, int bn) {
+    //cout << an << " " << bn << endl;
+    memset(dp, 0, sizeof dp);
+    for(int i=1; i<=an; i++) {
+        for(int j=1; j<=bn; j++) {
+            int p = i-1, q = j-1;
+            if(a[p] == b[q]) dp[i][j] = dp[i-1][j-1] + 1;
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    forn(i, bn+1) {
+        forn(j, an+1) {
+      //      cout << dp[j][i] << " ";
+        }
+      //  cout << endl;
+    }
+    return dp[an][bn];
+}
+
+int main(){
+
+    int t;
+    scanf("%d",&t);
+
+    while(t--) {
+        int an, bn;
+        memset(a, 0, sizeof a);
+        int num, i=0;
+        do{
+            cin >> num;
+            a[i] = num;
+        //    cout << "i " << i << endl;
+            i++;
+        }while(num);
+        an = i-1;
+
+        int maxx = 0;
+        while(1) {
+            int first;
+            cin >> first;
+            if(first == 0) break;
+            b[0] = first;
+            i = 1;
+            do{
+                cin >> first;
+                b[i] = first;
+                i++;
+            }while(first);
+            bn = i-1;
+            maxx = max(maxx, lcs(an, bn));
+        }
+        cout << maxx << endl;
+    }
+    return 0;
+}
